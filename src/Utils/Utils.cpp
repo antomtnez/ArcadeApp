b024dea8424dfaba27aa4ca57756839067de9d6f
Antonio Martinez <antomartinezdev@gmail.com>
2023-10-30 21:01:51 +0100

    Create Utils class
    An Utils class with operators for vector2 class

---
+++ src/Utils/Utils.cpp
@@ -0,0 +1,27 @@
+/*
+ * Utils.cpp
+ *
+ *  Created on: 30 oct 2023
+ *      Author: Anto Martinez
+ */
+
+#include "Utils.h"
+#include <cmath>
+
+static const float EPSILON = 0.0001f;
+
+bool IsEqual(float x, float y)
+{
+	return fabsf(x - y) < EPSILON;
+}
+
+bool IsGreaterThanOrEqual(float x, float y)
+{
+	return x > y || IsEqual(x, y);
+}
+
+bool IsLessThanOrEqual(float x, float y)
+{
+	return x < y || IsEqual(x, y);
+}
+
---
+++ src/Utils/Utils.h
@@ -0,0 +1,18 @@
+/*
+ * Utils.h
+ *
+ *  Created on: 30 oct 2023
+ *      Author: Anto Martinez
+ */
+
+#ifndef UTILS_UTILS_H_
+#define UTILS_UTILS_H_
+
+bool IsEqual(float x, float y);
+
+bool IsGreaterThanOrEqual(float x, float y);
+
+bool IsLessThanOrEqual(float x, float y);
+
+
+#endif /* UTILS_UTILS_H_ */
